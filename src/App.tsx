e98dda850811872ddf5105d468e42c8482c458fb
zhinong <651507008@qq.com>
2021-01-13 12:32:10 +0800

    添加图片

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,69 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components'
+import Nav from 'components/Nav'
+
+
+const Wrapper = styled.div`
+ height:100vh;
+  display:flex;
+  flex-direction:column;
+`;
+const Main = styled.div`
+  flex-grow:1;
+  overflow:auto;
+` ;
 
 
 
-
-function App() {
+export default function App() {
   return (
-    <div >
-      hello world
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch >
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+        <Redirect exact from="/" to="money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+        </Main>
+        <Nav />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+     
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function NoMatch() {
+  return <h2>当前页面不存在</h2>;
+}
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+

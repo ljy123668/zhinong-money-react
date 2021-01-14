de58f5bec0891de84158e4eb003a3737bab8abd8
zhinong <651507008@qq.com>
2021-01-14 12:20:56 +0800

    NoteSection UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -26,6 +26,31 @@ const TagSection = styled.section`
         margin-top:8px;
     }
 `
+const NoteSection = styled.section`
+    background-color:#f5f5f5;
+    padding:0 16px;
+    font-size:14px;
+    > label{
+        display: flex;
+        align-items:center;
+        > span{
+            margin-right:16px;
+            white-space:nowrap;
+        }
+        > input{
+            display:block;
+            width:100%;
+            height:72px;
+            background:none;
+            border:none;
+        }
+    }
+`
+const CategorySection = styled.section`
+`
+
+const NumberPadSection = styled.section`
+`
 
 
 function Money() {
@@ -40,27 +65,35 @@ function Money() {
                 </ol>
                 <button>新增标签</button>
             </TagSection>
-            <section>
-                备注
-            </section>
-            <section>
-                支出 收入
-            </section>
-            <section>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清除</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>OK</button>
-                <button>0</button>
-            </section>
+            <NoteSection>
+                <label><span>备注</span><input type="text" placeholder="在这里添加备注" /></label>
+            </NoteSection>
+            <CategorySection>
+                <ul>
+                    <li>支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
+                <div>
+                    100
+                </div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>删除</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清除</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>OK</button>
+                    <button>0</button>
+                </div>
+            </NumberPadSection>
         </Layout>
     )
 }

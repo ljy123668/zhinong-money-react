74cf153f2aae51c9bcd30ff77790f4c1a60b47e8
zhinong <651507008@qq.com>
2021-01-15 11:43:59 +0800

    创建Tag 路由 学会exact 模糊匹配

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Tags from 'views/Tags'
 import Money from 'views/Money'
 import Statistics from 'views/Statistics'
 import NoMatch from 'views/NoMatch'
+import { Tag } from 'views/Tag'
 import styled from 'styled-components'
 
 const AppWrapper = styled.div`
@@ -22,13 +23,16 @@ export default function App() {
       <Router>
 
         <Switch >
-          <Route path="/tags">
+          <Route exact path="/tags">
             <Tags />
           </Route>
-          <Route path="/money">
+          <Route exact path="/tags/:tag">
+            <Tag />
+          </Route>
+          <Route exact path="/money">
             <Money />
           </Route>
-          <Route path="/statistics">
+          <Route exact path="/statistics">
             <Statistics />
           </Route>
           <Redirect exact from="/" to="money" />

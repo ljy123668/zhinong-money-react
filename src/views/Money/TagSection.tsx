f82a1fa08e1fc8f716b9ab2e57682faeb0e96c80
zhinong <651507008@qq.com>
2021-01-16 10:26:40 +0800

    完成标签数据持久化

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import React from "react";
 import { useTags } from 'useTags';
-import { createId } from "lib/createId";
+
 
 const Wrapper = styled.section`
 background-color: #fff;
@@ -41,15 +41,10 @@ type Props = {
     onChange: (selected: number[]) => void
 }
 const TagsSection: React.FC<Props> = (props) => {
-    const { tags, setTags } = useTags()
+    const { tags, addTag } = useTags()
 
     const selectedTagIds = props.value
-    const onAddTag = () => {
-        const tagName = window.prompt('新标签的名称为')
-        if (tagName !== null) {
-            setTags([...tags, { id: createId(), name: tagName }])
-        }
-    }
+
     const onToggleTag = (tagId: number) => {
         const index = selectedTagIds.indexOf(tagId)
         if (index >= 0) {
@@ -67,7 +62,7 @@ const TagsSection: React.FC<Props> = (props) => {
                     >{tag.name}</li>
                 )}
             </ol>
-            <button onClick={onAddTag}>新增标签</button>
+            <button onClick={addTag}>新增标签</button>
         </Wrapper>
     )
 }
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -28,7 +28,7 @@ const TagList = styled.ol`
 
 
 function Tags() {
-    const { tags, setTags } = useTags()
+    const { tags, addTag } = useTags()
 
     return (
         <Layout>
@@ -47,7 +47,7 @@ function Tags() {
                 <Space />
                 <Space />
                 <Space />
-                <Button>新增标签</Button>
+                <Button onClick={addTag}>新增标签</Button>
                 <Space />
             </Center>
         </Layout>
